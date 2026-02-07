b5f09af711a241efe14bd829e3e1349c783ed0f6
Mnemotechnician <69920617+mnemotechnician@users.noreply.github.com>
2026-02-08 01:12:38 +0300

    Fix default zoom in step

--- Content.Shared/_Floof/CCVar/FloofCCVars.FOV.cs
+++ Content.Shared/_Floof/CCVar/FloofCCVars.FOV.cs
@@ -8,7 +8,7 @@ public sealed partial class FloofCCVars
     ///     The number by which the current FOV size is mulltiplied when zooming in.
     /// </summary>
     public static readonly CVarDef<float> ZoomInStep =
-        CVarDef.Create("fov.zoom_in_step", 1.833f, CVar.CLIENT | CVar.REPLICATED | CVar.ARCHIVE);
+        CVarDef.Create("fov.zoom_in_step", 0.8f, CVar.CLIENT | CVar.REPLICATED | CVar.ARCHIVE);
 
     /// <summary>
     ///     The number by which the current FOV size is multiplied when zooming out.
